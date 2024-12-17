5d737750eb025c2b756a2daa6de74dc1aa93a7e7
Juan Carlos Fernandez del Castillo <jfernandezdelcasti@holylogin07.rc.fas.harvard.edu>
2024-12-17 13:09:51 -0500

    initial commit

---
+++ run_optimization.py
@@ -0,0 +1,41 @@
+from canonical_model_jax import *
+import argparse 
+
+def phi(u): 
+    return 1 / (1 + jnp.exp(-u))
+
+def psi(x): 
+    return jnp.log(x / (1 - x))
+
+rho_vectorized = jax.vmap(lambda x: compute_rho(x, tol=1e-10), in_axes=0)
+
+def run_optimization(N, n, M, sigma_c, output_dir): 
+    P = 1000
+    key = jax.random.PRNGKey(0)
+    W_init = 1 / jnp.sqrt(N) * jax.random.gamma(key, a=1, shape=(M, N))
+    gamma = 1
+    os = OlfactorySensing(N=N, n=n, M=M, P=P, sigma_c=sigma_c)
+    os.W = W_init
+    os.cs = os.draw_cs(key=key)     
+    Ws, ents, losses = natural_gradient_dual_space(10, W_init, os.cs, key, lambda * args: - os.log_det_sigma(*args), 1, os, phi, psi)
+    rhos = rho_vectorized(Ws)
+    fig = plot_trajectory(Ws, ents, losses, rhos, sigma_c=sigma_c, gamma=gamma)
+    filename = f"{output_dir}/sigma_{sigma_c:.2g}_N_{N}_n_{n}_M_{M}"
+    fig.savefig(filename + '.png')
+    jnp.save(filename + '.npy', Ws) 
+    return rhos[-1]
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--sigma", type=float, required=False, help="Input sigma value", default=2.0)
+    parser.add_argument("--n", type=int, required=False, help="Input n", default=2) 
+    parser.add_argument("--N", type=int, required=False, help="Input N", default=100) 
+    parser.add_argument("--M", type=int, required=False, help="Input M", default=30) 
+    parser.add_argument("--output_dir", type=str, required=True, help="Output directory")
+    parser.add_argument("--results_file", type=str, required=True, help="Path to results file")
+    args = parser.parse_args()
+
+    rho = run_optimization(args.N, args.n, args.M, args.sigma, args.output_dir)
+
+    with open(args.results_file, "a") as f:
+        f.write(f"{args.N},{args.n},{args.M},{args.sigma},{rho}\n")
