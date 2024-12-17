2054a993c255f1707df0a0158552cbb90220d141
Juan Carlos Fernandez del Castillo <jfernandezdelcasti@boslogin07.rc.fas.harvard.edu>
2024-12-17 17:05:44 -0500

    clip initialization to max < 1

--- run_optimization.py
+++ run_optimization.py
@@ -12,12 +12,12 @@ rho_vectorized = jax.vmap(lambda x: compute_rho(x, tol=1e-10), in_axes=0)
 def run_optimization(N, n, M, sigma_c, output_dir): 
     P = 1000
     key = jax.random.PRNGKey(0)
-    W_init = 1 / jnp.sqrt(N) * jax.random.gamma(key, a=1, shape=(M, N))
+    W_init = jnp.clip(1 / jnp.sqrt(N) * jax.random.gamma(key, a=1, shape=(M, N)), a_min=0, a_max=(1 - 1e-10)) 
     gamma = 1
     os = OlfactorySensing(N=N, n=n, M=M, P=P, sigma_c=sigma_c)
     os.W = W_init
     os.cs = os.draw_cs(key=key)     
-    Ws, ents, losses = natural_gradient_dual_space(10, W_init, os.cs, key, lambda * args: - os.log_det_sigma(*args), 1, os, phi, psi)
+    Ws, ents, losses = natural_gradient_dual_space(500, W_init, os.cs, key, lambda * args: - os.log_det_sigma(*args), 1, os, phi, psi)
     rhos = rho_vectorized(Ws)
     fig = plot_trajectory(Ws, ents, losses, rhos, sigma_c=sigma_c, gamma=gamma)
     filename = f"{output_dir}/sigma_{sigma_c:.2g}_N_{N}_n_{n}_M_{M}"
@@ -38,4 +38,4 @@ if __name__ == '__main__':
     rho = run_optimization(args.N, args.n, args.M, args.sigma, args.output_dir)
 
     with open(args.results_file, "a") as f:
-        f.write(f"{args.N},{args.n},{args.M},{args.sigma},{rho}\n")
+        f.write(f"{args.N}\t{args.n}\t{args.M}\t{args.sigma}\t{rho}\n")
