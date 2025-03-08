62ad3f250c01ee73ac02ef6626d5ddafaf7934cc
Juan Carlos Fernandez del Castillo <jfernandezdelcasti@boslogin05.rc.fas.harvard.edu>
2025-03-08 13:49:36 -0500

    update for hyperparameter sweep

--- rbm/make_train_args.py
+++ rbm/make_train_args.py
@@ -1,13 +1,20 @@
 import numpy as np
 import json 
 
-args = json.load(open('train_args.json', 'r'))
+args = json.load(open('/n/home10/jfernandezdelcasti/noncanonical-olfaction/rbm/train_args.json', 'r'))
 
-args['epochs'] = 20
+args['epochs'] = 200
+args['batch_size'] = 10
+args['sample_number'] = 10000
 
-l2_regs = np.logspace(-4, -1, 13) 
+l2_regs = np.logspace(-4, -1, 7) 
+gammas = np.logspace(-4, -1, 7)
 
+k = 0
 for i, reg in enumerate(l2_regs): 
     args['l2_reg'] = reg
-    with open(f'train_args_{i}.json', 'w') as out: 
-        json.dump(args, out) 
+    for j, gamma in enumerate(gammas): 
+        args['learning_rate'] = gamma 
+        with open(f'train_args_{k}.json', 'w') as out: 
+            json.dump(args, out) 
+            k += 1
--- rbm/train_RBM.py
+++ rbm/train_RBM.py
@@ -7,14 +7,22 @@ import shutil
 
 # read arguments and copy training arguments into output directory 
 job_id = sys.argv[1] 
-arg_dict_path = sys.argv[2] 
-
-os.mkdir(job_id)
+arg_dict_path = sys.argv[2]
 
 with open(arg_dict_path, 'r') as adp: 
     train_args = json.load(adp)
 
-parameter_path = os.path.join(job_id, 'parameters.json') 
+
+reg = train_args['l2_reg']
+lr = train_args['learning_rate']
+lambda_exp = jnp.log10(reg) 
+lr_exp = jnp.log10(lr) 
+
+output_dir_name = f"gamma_lambda_sweep/lambda_${lambda_exp:.2f}_gamma_${lr_exp:.2f}$"
+os.mkdir(output_dir_name)
+
+
+parameter_path = os.path.join(output_dir_name, 'parameters.json') 
 
 shutil.copy2(arg_dict_path, parameter_path) 
 
@@ -23,21 +31,23 @@ rbm = GenerativeRBM(n_hidden=128)
 fig, axs, samples, err = rbm.plot_deviations_over_time(train_args)
 
 # create output paths and write outputs 
-deviation_numpy_path = os.path.join(job_id, 'moment_deviations') 
-deviation_png_path = os.path.join(job_id, 'moment_deviations.png')
+deviation_numpy_path = os.path.join(output_dir_name, 'moment_deviations') 
+deviation_png_path = os.path.join(output_dir_name, 'moment_deviations.png')
 
 jnp.save(deviation_numpy_path, err) 
-fig.savefig(deviation_png_path) 
-
-samples_numpy_path = os.path.join(job_id, 'samples') 
-
-samples_png_path = os.path.join(job_id, 'samples.png') 
-
-
-fig, ax = rbm.plot_samples(samples) 
 
 reg = train_args['l2_reg']
 exp = jnp.log10(reg) 
+
+fig.suptitle(rf'$10^{{{exp:.2f}}}$', size=40) 
+
+fig.savefig(deviation_png_path) 
+
+# make samples output paths
+samples_numpy_path = os.path.join(output_dir_name, 'samples') 
+samples_png_path = os.path.join(output_dir_name, 'samples.png') 
+
+fig, ax = rbm.plot_samples(samples) 
 
 fig.suptitle(rf'$10^{{{exp:.2f}}}$', size=40) 
 
