7a8553efc3191c18b2d1e95ab70e1a7d9b0ea969
Juan Carlos Fernandez del Castillo <jfernandezdelcasti@boslogin06.rc.fas.harvard.edu>
2025-03-10 11:37:05 -0400

    make n_hidden a hyperparameter

--- rbm/make_hyperparameters.py
+++ rbm/make_hyperparameters.py
@@ -3,9 +3,10 @@ import json
 
 args = json.load(open('/n/home10/jfernandezdelcasti/noncanonical-olfaction/rbm/train_args.json', 'r'))
 
-args['epochs'] = 200
+args['epochs'] = 20
 args['batch_size'] = 10
-args['sample_number'] = 10000
+args['sample_number'] = 1000
+args['n_hidden'] = 500
 
 l2_regs = np.logspace(-4, -1, 7) 
 gammas = np.logspace(-4, -1, 7)
@@ -15,6 +16,6 @@ for i, reg in enumerate(l2_regs):
     args['l2_reg'] = reg
     for j, gamma in enumerate(gammas): 
         args['learning_rate'] = gamma 
-        with open(f'train_args_{k}.json', 'w') as out: 
+        with open(f'train_args/train_args_{k}.json', 'w') as out: 
             json.dump(args, out) 
             k += 1
